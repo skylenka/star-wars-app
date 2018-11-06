e4cda69021f6d12e5f1a422aa94158bc6583be73
Magdalena Olszewska <lenka@MacBook-Pro-Magdalena.local>
2018-11-06 22:36:07 +0100

    first page for starships

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { List } from 'semantic-ui-react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    starships: [
+      {
+        name: '',
+        url: '',
+      },
+    ],
+  }
+
+  componentDidMount() {
+    fetch('https://swapi.co/api/starships/')
+      .then(resp => resp.json())
+      .then(({ results }) => {
+        this.setState({
+          starships: results,
+        })
+      })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <List>
+        {this.state.starships.map(starship => (
+          <List.Item key={starship.url}>{starship.name}</List.Item>
+        ))}
+      </List>
+    )
   }
 }
 
