8f7aa24346b6eb1112a75efee2b0f8f893b2b242
Magdalena Olszewska <lenka@MacBook-Pro-Magdalena.local>
2018-11-08 00:31:13 +0100

    added card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { List } from 'semantic-ui-react';
-import './App.css';
+import { List, Card } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
 
 class App extends Component {
   state = {
     starships: [
       {
         name: '',
+        model: '',
+        manufacturer: '',
         url: '',
       },
     ],
@@ -24,11 +26,19 @@ class App extends Component {
 
   render() {
     return (
-      <List>
-        {this.state.starships.map(starship => (
-          <List.Item key={starship.url}>{starship.name}</List.Item>
-        ))}
-      </List>
+        <Card.Group>
+          {this.state.starships.map(starship => (
+            <Card key={starship.url}>
+              <Card.Content>
+                <Card.Header>{starship.name}</Card.Header>
+                <Card.Meta>{starship.model}t</Card.Meta>
+                <Card.Description>
+                {starship.manufacturer}
+                </Card.Description>
+              </Card.Content>
+            </Card>
+          ))}
+        </Card.Group>
     )
   }
 }
