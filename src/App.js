ff4bbca67f59bb855fa61c888df956569f3ce412
Magdalena Olszewska <lenka@MacBook-Pro-Magdalena.local>
2018-11-09 00:36:04 +0100

    change components structure

--- src/App.js
+++ src/App.js
@@ -1,44 +1,13 @@
 import React, { Component } from 'react';
-import { List, Card } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
+import Starships from './components/cards/Starships.js';
 
 class App extends Component {
-  state = {
-    starships: [
-      {
-        name: '',
-        model: '',
-        manufacturer: '',
-        url: '',
-      },
-    ],
-  }
 
-  componentDidMount() {
-    fetch('https://swapi.co/api/starships/')
-      .then(resp => resp.json())
-      .then(({ results }) => {
-        this.setState({
-          starships: results,
-        })
-      })
-  }
 
   render() {
     return (
-        <Card.Group>
-          {this.state.starships.map(starship => (
-            <Card key={starship.url}>
-              <Card.Content>
-                <Card.Header>{starship.name}</Card.Header>
-                <Card.Meta>{starship.model}t</Card.Meta>
-                <Card.Description>
-                {starship.manufacturer}
-                </Card.Description>
-              </Card.Content>
-            </Card>
-          ))}
-        </Card.Group>
+        <Starships />
     )
   }
 }
---
+++ src/components/cards/Starships.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Card } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+
+class Starships extends Component {
+    state = {
+      starships: [
+        {
+          name: '',
+          model: '',
+          manufacturer: '',
+          url: '',
+        },
+      ],
+    }
+  
+    componentDidMount() {
+      fetch('https://swapi.co/api/starships/')
+        .then(resp => resp.json())
+        .then(({ results }) => {
+          this.setState({
+            starships: results,
+          })
+        })
+    }
+  
+    render() {
+      return (
+          <Card.Group>
+            {this.state.starships.map(starship => (
+              <Card key={starship.url}>
+                <Card.Content>
+                  <Card.Header>{starship.name}</Card.Header>
+                  <Card.Meta>{starship.model}t</Card.Meta>
+                  <Card.Description>
+                  {starship.manufacturer}
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+            ))}
+          </Card.Group>
+      )
+    }
+  }
+  
+  export default Starships;
