f1de38fbd41fb87919c743df008a2da4aea1c596
Magdalena Olszewska <magdalena.monika.olszewska@gmail.com>
2018-11-10 15:03:03 +0100

    added activeItems

--- src/App.js
+++ src/App.js
@@ -42,23 +42,42 @@ function Planets() {
 }
 
 class App extends Component {
+  state = {}
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
   render() {
+    const { activeItem } = this.state
+
     return (
       <div>
         <Router>
       <div>
         <Menu>
-          <Menu.Item>
+          <Menu.Item
+            name='home'
+            active={activeItem === 'home'}
+            onClick={this.handleItemClick}
+          >
             <Link to="/">Home</Link>
           </Menu.Item>
-          <Menu.Item>
+          <Menu.Item
+          name='spacecrafts'
+          active={activeItem === 'spacecrafts'}
+          onClick={this.handleItemClick}
+          >
             <Link to="/spacecrafts">Spacecrafts</Link>
           </Menu.Item>
-          <Menu.Item>
+          <Menu.Item
+          name='people'
+          active={activeItem === 'people'}
+          onClick={this.handleItemClick}>
             <Link to="/people">People</Link>
           </Menu.Item>
-          <Menu.Item>
+          <Menu.Item
+          name='planets'
+          active={activeItem === 'planets'}
+          onClick={this.handleItemClick}>
             <Link to="/planets">Planets</Link>
           </Menu.Item>
         </Menu>
