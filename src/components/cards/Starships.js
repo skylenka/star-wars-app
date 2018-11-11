7d51261e64e274b5ad85e71762c058fec1c389d9
Magdalena Olszewska <magdalena.monika.olszewska@gmail.com>
2018-11-11 23:40:12 +0100

    starships next page

--- src/components/cards/Starships.js
+++ src/components/cards/Starships.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Button } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 
 class Starships extends Component {
     state = {
+      curr: 'https://swapi.co/api/starships/',
+      prev: '',
+      next: '',
       starships: []
     }
   
     componentDidMount() {
-      fetch('https://swapi.co/api/starships/')
+      fetch(this.state.curr)
         .then(resp =>  {
           if (resp.ok)
           return resp.json();
           else
           throw new Error ('Błąd seci!');
-        }).then(({ results }) => {
+        }).then(({ results, next, previous }) => {
           this.setState({
             starships: results,
+            next: next,
+            prev: previous,
           });
         }).catch(err => {
           console.log(err)
@@ -25,19 +30,24 @@ class Starships extends Component {
   
     render() {
       return (
-          <Card.Group>
-            {this.state.starships.map(starship => (
-              <Card key={starship.url}>
-                <Card.Content>
-                  <Card.Header>{starship.name}</Card.Header>
-                  <Card.Meta>{starship.model}</Card.Meta>
-                  <Card.Description>
-                  {starship.manufacturer}
-                  </Card.Description>
-                </Card.Content>
-              </Card>
-            ))}
-          </Card.Group>
+        <div>
+            <Button content='Prev' icon='left arrow' labelPosition='left' size='mini'/>
+            <Button content='Next' icon='right arrow' labelPosition='right' size='mini' onClick={ () => this.setState({url:this.state.next}) } />
+            <hr />
+            <Card.Group>
+              {this.state.starships.map(starship => (
+                <Card key={starship.url}>
+                  <Card.Content>
+                    <Card.Header>{starship.name}</Card.Header>
+                    <Card.Meta>{starship.model}</Card.Meta>
+                    <Card.Description>
+                    {starship.manufacturer}
+                    </Card.Description>
+                  </Card.Content>
+                </Card>
+              ))}
+            </Card.Group>
+          </div>
       )
     }
   }
