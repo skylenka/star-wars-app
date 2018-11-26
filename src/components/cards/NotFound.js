5e80ef2756f974336640e01488d4939ac141c5ca
Magdalena Olszewska <magdalena.monika.olszewska@gmail.com>
2018-11-26 20:08:38 +0100

    component for 404 page

---
+++ src/components/cards/NotFound.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import 'semantic-ui-css/semantic.min.css';
+
+class NotFound extends Component {
+  render() {
+    return <h2>404</h2>;
+  }
+}
+
+export default NotFound;
