257107caa8a229770ffc1a02c0c1c99a06dde229
Magdalena Olszewska <magdalena.monika.olszewska@gmail.com>
2019-01-08 13:39:31 +0100

    app fix after cr

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import Starships from './components/Starships.js';
 import People from './components/People.js';
@@ -8,50 +8,39 @@ import NotFound from './components/NotFound.js';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Menu } from 'semantic-ui-react';
 
-function MenuLink({ name, to, activeOnlyWhenExact }) {
-  return (
-    <Route
-      path={to}
-      exact={activeOnlyWhenExact}
-      children={({ match }) => (
-        <Menu.Item className={match ? 'active' : ''}>
-          <Link to={to}>{name}</Link>
-        </Menu.Item>
-      )}
-    />
-  );
-}
+const MenuLink = ({ name, to, activeOnlyWhenExact }) => (
+  <Route
+    path={to}
+    exact={activeOnlyWhenExact}
+    children={({ match }) => (
+      <Menu.Item className={match ? 'active' : ''}>
+        <Link to={to}>{name}</Link>
+      </Menu.Item>
+    )}
+  />
+);
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <Router>
-          <div>
-            <Menu>
-              <MenuLink
-                activeOnlyWhenExact={true}
-                name="home"
-                as={Link}
-                to="/"
-              />
-              <MenuLink name="spacecrafts" as={Link} to="/spacecrafts" />
-              <MenuLink name="people" as={Link} to="/people" />
-              <MenuLink name="planets" as={Link} to="/planets" />
-            </Menu>
+const App = () => (
+  <>
+    <Router>
+      <div>
+        <Menu>
+          <MenuLink activeOnlyWhenExact={true} name="home" as={Link} to="/" />
+          <MenuLink name="spacecrafts" as={Link} to="/spacecrafts" />
+          <MenuLink name="people" as={Link} to="/people" />
+          <MenuLink name="planets" as={Link} to="/planets" />
+        </Menu>
 
-            <hr />
+        <hr />
 
-            <Route exact path="/" component={Home} />
-            <Route path="/spacecrafts" component={Starships} />
-            <Route path="/people" component={People} />
-            <Route path="/planets" component={Planet} />
-            <Route path="*" component={NotFound} />
-          </div>
-        </Router>
-      </>
-    );
-  }
-}
+        <Route exact path="/" component={Home} />
+        <Route path="/spacecrafts" component={Starships} />
+        <Route path="/people" component={People} />
+        <Route path="/planets" component={Planet} />
+        <Route path="*" component={NotFound} />
+      </div>
+    </Router>
+  </>
+);
 
 export default App;
