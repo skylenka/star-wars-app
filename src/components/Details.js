1ba74a2eaaa307e46b86d332f8d17ad6141d9e63
Magdalena Olszewska <magdalena.monika.olszewska@gmail.com>
2019-02-02 18:54:54 +0100

    match in class component

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,5 +1,23 @@
 import React from 'react';
 
-const Details = ({ match }) => <h3>ID: {match.params.id}</h3>;
+class Details extends React.Component {
+  state = {
+    id: this.props.match.params.id,
+    link: `https://swapi.co/api/people/${this.props.match.params.id}`,
+    content: null
+  };
+
+  componentDidMount() {
+    fetch(this.state.link)
+      .then(resp => resp.json())
+      .then(({ results }) => {
+        this.setState({ content: results });
+      });
+  }
+
+  render() {
+    return <p>cokolwiek</p>;
+  }
+}
 
 export default Details;
