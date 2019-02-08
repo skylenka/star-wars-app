46897201eeaae8360db1c4f11c0a01fb1cb28d75
Magdalena Olszewska <[magdalena.monika.olszewska@gmail.com]>
2019-02-08 12:28:26 +0100

    test commit with logo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Container, Image } from 'semantic-ui-react';
 import logo from '../logo.svg';
-import swlogo from '../logosw.png';
+
 
 const Home = () => (
   <>
     <Container fluid>
-      <Image src={swlogo} size="small" centered />
+      
       <Image src={logo} size="small" centered />
+      <div class="🤖"></div>
+<div class="後">Star wars</div> 
     </Container>
   </>
 );
