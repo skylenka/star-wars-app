33bcd6b15d05294807e8316188ab2232fb77acd9
Magdalena Olszewska <magdalena.monika.olszewska@gmail.com>
2019-02-20 21:56:28 +0100

    added extra page link

--- src/components/ExtraCard.js
+++ src/components/ExtraCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Feed } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 class ExtraCard extends React.Component {
   constructor(props) {
@@ -18,16 +19,21 @@ class ExtraCard extends React.Component {
         this.setState({ content: resp });
       });
   }
-
   render() {
     return (
       <>
         <Feed.Event>
           <Feed.Content>
             <Feed.Summary>
-              {this.state.content.title
-                ? this.state.content.title
-                : this.state.content.name}
+              {this.state.content.title ? (
+                <Link to={`details/${this.state.link.match(/\/api\/(.*)/)[1]}`}>
+                  {this.state.content.title}
+                </Link>
+              ) : (
+                <Link to={`details/${this.state.link.match(/\/api\/(.*)/)[1]}`}>
+                  {this.state.content.name}
+                </Link>
+              )}
             </Feed.Summary>
           </Feed.Content>
         </Feed.Event>
