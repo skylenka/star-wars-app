fc40a96a80bd56150e93c1141274c46dca53979c
Magdalena Olszewska <magdalena.monika.olszewska@gmail.com>
2019-03-03 11:41:21 +0100

    update search component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import Starships from './components/Starships.js';
 import People from './components/People.js';
@@ -22,7 +22,7 @@ import {
   Switch,
   Redirect
 } from 'react-router-dom';
-import { Menu, Dropdown } from 'semantic-ui-react';
+import { Menu, Dropdown, Input } from 'semantic-ui-react';
 
 const MenuLink = ({ name, to, activeOnlyWhenExact }) => (
   <Route
@@ -36,58 +36,92 @@ const MenuLink = ({ name, to, activeOnlyWhenExact }) => (
   />
 );
 
-const App = () => (
-  <>
-    <Router>
-      <div style={{ margin: '10px' }}>
-        <Menu inverted>
-          <MenuLink activeOnlyWhenExact={true} name="Home" as={Link} to="/" />
-          <Dropdown text="Elements" pointing className="link item">
-            <Dropdown.Menu>
-              <Dropdown.Header>Categories</Dropdown.Header>
-              <Dropdown.Item as={Link} to="/starships">
-                Starships
-              </Dropdown.Item>
-              <Dropdown.Item as={Link} to="/people">
-                People
-              </Dropdown.Item>
-              <Dropdown.Item as={Link} to="/planets">
-                Planets
-              </Dropdown.Item>
-              <Dropdown.Item as={Link} to="/films">
-                Films
-              </Dropdown.Item>
-              <Dropdown.Item as={Link} to="/species">
-                Species
-              </Dropdown.Item>
-              <Dropdown.Item as={Link} to="/vehicles">
-                Vehicles
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-          <MenuLink name="Search" as={Link} to="/search" />
-        </Menu>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Redirect from="/star-wars-app" to="/" />
-          <Route path="/starships" component={Starships} />
-          <Route path="/people" component={People} />
-          <Route path="/planets" component={Planet} />
-          <Route path="/films" component={Films} />
-          <Route path="/species" component={Species} />
-          <Route path="/vehicles" component={Vehicles} />
-          <Route path="/search" component={Search} />
-          <Route path="/details/people/:id?" component={PeopleDetails} />
-          <Route path="/details/planets/:id?" component={PlanetsDetails} />
-          <Route path="/details/starships/:id?" component={StarshipsDetails} />
-          <Route path="/details/films/:id?" component={FilmsDetails} />
-          <Route path="/details/species/:id?" component={SpeciesDetails} />
-          <Route path="/details/vehicles/:id?" component={VehiclesDetails} />
-          <Route path="*" component={NotFound} />
-        </Switch>
-      </div>
-    </Router>
-  </>
-);
+class App extends Component {
+  state = {
+    value: '',
+    query: ''
+  };
+
+  handleChange = event => {
+    this.setState({ value: event.target.value });
+  };
+
+  handleClick = event => {
+    this.setState({
+      query: `https://swapi.co/api/people/?search=${this.state.value}`
+    });
+  };
+  render() {
+    return (
+      <>
+        <Router>
+          <div style={{ margin: '10px' }}>
+            <Menu inverted>
+              <MenuLink
+                activeOnlyWhenExact={true}
+                name="Home"
+                as={Link}
+                to="/"
+              />
+              <Dropdown text="Elements" pointing className="link item">
+                <Dropdown.Menu>
+                  <Dropdown.Header>Categories</Dropdown.Header>
+                  <Dropdown.Item as={Link} to="/starships">
+                    Starships
+                  </Dropdown.Item>
+                  <Dropdown.Item as={Link} to="/people">
+                    People
+                  </Dropdown.Item>
+                  <Dropdown.Item as={Link} to="/planets">
+                    Planets
+                  </Dropdown.Item>
+                  <Dropdown.Item as={Link} to="/films">
+                    Films
+                  </Dropdown.Item>
+                  <Dropdown.Item as={Link} to="/species">
+                    Species
+                  </Dropdown.Item>
+                  <Dropdown.Item as={Link} to="/vehicles">
+                    Vehicles
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+              <Menu.Menu position="right">
+                <Menu.Item>
+                  <Input icon="search" placeholder="Search..." />
+                </Menu.Item>
+                <Menu.Item name="Find" as={Link} to="/search" />
+              </Menu.Menu>
+            </Menu>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Redirect from="/star-wars-app" to="/" />
+              <Route path="/starships" component={Starships} />
+              <Route path="/people" component={People} />
+              <Route path="/planets" component={Planet} />
+              <Route path="/films" component={Films} />
+              <Route path="/species" component={Species} />
+              <Route path="/vehicles" component={Vehicles} />
+              <Route path="/search" component={Search} />
+              <Route path="/details/people/:id?" component={PeopleDetails} />
+              <Route path="/details/planets/:id?" component={PlanetsDetails} />
+              <Route
+                path="/details/starships/:id?"
+                component={StarshipsDetails}
+              />
+              <Route path="/details/films/:id?" component={FilmsDetails} />
+              <Route path="/details/species/:id?" component={SpeciesDetails} />
+              <Route
+                path="/details/vehicles/:id?"
+                component={VehiclesDetails}
+              />
+              <Route path="*" component={NotFound} />
+            </Switch>
+          </div>
+        </Router>
+      </>
+    );
+  }
+}
 
 export default App;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,63 +1,31 @@
-import React, { Component } from 'react';
-import { Input, Button } from 'semantic-ui-react';
+import React from 'react';
 import ListRenderer from './ListRenderer';
 import { Card } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
-class Search extends Component {
-  state = {
-    value: '',
-    query: ''
-  };
-
-  handleChange = event => {
-    this.setState({ value: event.target.value });
-  };
-
-  handleClick = event => {
-    this.setState({
-      query: `https://swapi.co/api/people/?search=${this.state.value}`
-    });
-  };
-  render() {
-    return (
-      <>
-        <Input
-          placeholder="Search..."
-          value={this.state.value}
-          onChange={this.handleChange}
-        />
-        <Button
-          style={{ margin: '0px 0px 0px 10px' }}
-          positive
-          onClick={this.handleClick}
-        >
-          Go!
-        </Button>
-
-        <ListRenderer
-          name="People"
-          curr={this.state.query}
-          renderCard={person => (
-            <Card key={person.url}>
-              <Card.Content>
-                <Card.Header>{person.name}</Card.Header>
-                <Card.Meta>Height: {person.height}</Card.Meta>
-                <Card.Description>Birth: {person.birth_year}</Card.Description>
-                <Card.Description>Gender: {person.gender}</Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-                <div>
-                  <Link to={`details/${person.url.match(/\/api\/(.*)/)[1]}`}>
-                    Read more
-                  </Link>
-                </div>
-              </Card.Content>
-            </Card>
-          )}
-        />
-      </>
-    );
-  }
-}
+const Search = () => (
+  <>
+    <ListRenderer
+      name="People"
+      // curr={this.props.query}
+      renderCard={person => (
+        <Card key={person.url}>
+          <Card.Content>
+            <Card.Header>{person.name}</Card.Header>
+            <Card.Meta>Height: {person.height}</Card.Meta>
+            <Card.Description>Birth: {person.birth_year}</Card.Description>
+            <Card.Description>Gender: {person.gender}</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <div>
+              <Link to={`details/${person.url.match(/\/api\/(.*)/)[1]}`}>
+                Read more
+              </Link>
+            </div>
+          </Card.Content>
+        </Card>
+      )}
+    />
+  </>
+);
 
 export default Search;
